2360313e2bd6c0a89f8a8a6d72b9eac8b07ce6d6
Rostyslav <rostyslav.lesovyi@globallogic.com>
2016-07-22 18:15:45 +0300

    added location

--- app/src/main/java/com/example/JavaTest.java
+++ app/src/main/java/com/example/JavaTest.java
@@ -4,24 +4,76 @@ import java.io.IOException;
 
 public class JavaTest {
 
+	public static short location(byte x, byte y)
+	{
+		return (short) ((x << 8)|y);
+	}
+
+	public static byte getXFromLocation(short location)
+	{
+		return (byte)(location >> 8);
+	}
+
+	public static byte getYFromLocation(short location)
+	{
+		return (byte) (location&0xFF);
+	}
+
+	public static short moveLocation(short location, byte width, byte height, String direction)
+	{
+		byte x = getXFromLocation (location);
+		byte y = getYFromLocation (location);
+
+		switch (direction)
+		{
+			case "up":
+				if (y < height-1)
+				{
+					y+=1;
+				}
+				break;
+			case "down":
+				if (y > 0)
+				{
+					y-=1;
+				}
+				break;
+			case "left":
+				if (x > 0)
+				{
+					x-=1;
+				}
+				break;
+			case "right":
+				if (x < width - 1)
+				{
+					x+=1;
+				}
+				break;
+		}
+		return location (x, y);
+	}
 	public static void main(final String[] args) {
-		int width = 20, height = 6;
-		int currentx = 0, currenty = 5;
-		int currentx1 = 1, currenty1 = 2;
+		byte width = 20, height = 6;
+		short locationO = location ((byte) 1, (byte) 2);
+		short locationA = location ((byte) 3, (byte) 3);
 
 		boolean work = true;
 		while(work)
 		{
 			System.out.println();
 
-			for(int y = height-1; y >= 0; y--)
+			for(byte y = (byte) (height-1); y >= 0; y--)
 			{
-				for (int x = 0; x < width; x++)
+				for (byte x = 0; x < width; x++)
 				{
-					if (currentx == x && currenty == y) {
+					short location = location (x, y);
+					if (location == locationO)
+					{
 						System.out.print("o");
 					}
-					else if (currentx1 == x && currenty1 == y) {
+					else if (location == locationA)
+					{
 						System.out.print("a");
 					}
 					else
@@ -35,53 +87,34 @@ public class JavaTest {
 			
 			int letter = readKey();
 			switch (letter)
-			{   case 'k':
-					if (currenty1 >= 1)
+			{
+				case 'k':
+					short newLocation = moveLocation (locationA, width, height, "down");
+					if (newLocation != locationO)
 					{
-						currenty1 = currenty1 - 1;
+						locationA = newLocation;
 					}
 					break;
 				case 'j':
-					if (currentx1 >= 1)
-					{
-						currentx1 = currentx1 - 1;
-					}
+					locationA = moveLocation (locationA, width, height, "left");
 					break;
 				case 'l':
-					if (currentx1 < width - 1)
-					{
-						currentx1 = currentx1 + 1;
-					}
+					locationA = moveLocation (locationA, width, height, "right");
 					break;
 				case 'i':
-					if (currenty1 < height - 1)
-					{
-						currenty1 = currenty1 + 1;
-					}
+					locationA = moveLocation (locationA, width, height, "up");
 					break;
 				case 's':
-					if (currenty >= 1)
-					{
-						currenty = currenty - 1;
-					}
+					locationO = moveLocation (locationO, width, height, "down");
 					break;
 				case 'a':
-					if (currentx >= 1)
-					{
-						currentx = currentx - 1;
-					}
+					locationO = moveLocation (locationO, width, height, "left");
 					break;
 				case 'd':
-					if (currentx < width - 1)
-					{
-						currentx = currentx + 1;
-					}
+					locationO = moveLocation (locationO, width, height, "right");
 					break;
 				case 'w':
-					if (currenty < height - 1)
-					{
-						currenty = currenty + 1;
-					}
+					locationO = moveLocation (locationO, width, height, "up");
 					break;
 				case 'q':
 					work = false;
